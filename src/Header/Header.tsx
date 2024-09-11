6db08ffab15fbb78e7bab81b98672039df20340d
Onkar Vallal <okvallal200@gmail.com>
2024-09-11 21:21:07 +0530

    Initial commit

---
+++ src/Header/Header.tsx
@@ -0,0 +1,36 @@
+import { IconBrandSafari, IconBell, IconSettings,} from "@tabler/icons-react";
+import { Avatar, Indicator} from '@mantine/core';
+import NavLinks from "./NavLinks";
+
+const Header = () => {
+    return (
+        <div className="w-full bg-mine-shaft-950 px-6 text-white h-20 flex justify-between items-center">
+            <div className="flex gap-1.5 items-center text-bright-sun-400">
+                <IconBrandSafari className="h-10 w-10" stroke={1.8} />
+                <div className="text-2xl font-semibold">jobNimbus</div>
+            </div>
+           
+            <NavLinks />
+            <div className="flex gap-4 items-center gap-2">
+            
+                <div className="flex items-center gap-2">
+                    <div>Morbius</div>
+                    <Avatar src="avatar-9.png" alt="it's me" />
+                </div>
+                
+                <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+                    <Indicator color="bright-sun.4" size={8} offset={6} processing>
+                        <IconBell stroke={1.5}/>
+                    </Indicator>
+                </div>
+                
+                <div className="bg-mine-shaft-900 p-1.5 rounded-full"> 
+                    <IconSettings stroke={1.5}/>
+                </div>
+               
+            </div>
+        </div>
+    );
+}
+
+export default Header;
